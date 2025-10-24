0ff5ddf6b565ae08ea4cfaea346c81cba7787a49
jhoublessed <jonatasbarros137@gmail.com>
2025-10-24 19:51:54 +0000

    first steps to build a machine learning model

---
+++ modelo_de_treinamento.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import joblib
+import os
+from sklearn import model_selection, preprocessing,pipeline,linear_model, metrics
+
+
+def carregar_dados(caminho_arquivo = "historicoAcademico.csv"):
+    try:
+        if os.path.exists(caminho_arquivo):
+
+            df = pd.read_csv(caminho_arquivo, encoding="latin1",sep=',')
+
+            print("o arquivo foi carregado com sucesso")
+
+            return df
+        else:
+            print("o arquivo não foi encontrado dentro da pasta")
+
+            return None
+    except Exception as e:
+        print("erro inesperado ao caregar o arquivo")
+
+        return None
+    
+#--- chamar a função para armazenar o resultado ---#
+
+dados = carregar_dados()
+print(dados)
